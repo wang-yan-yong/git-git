a8d61b1f373b66426d29b70b1dd00424da567267
Layne <Layne@Wyy.com>
2022-09-19 21:46:23 +0800

    second commit

--- src/main/java/com/wyy/git/GitTest01.java
+++ src/main/java/com/wyy/git/GitTest01.java
@@ -3,5 +3,6 @@ package com.wyy.git;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("Hello-Git");
+        System.out.println("Hello-Git01");
     }
 }
